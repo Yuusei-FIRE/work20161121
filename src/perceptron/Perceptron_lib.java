c730f15c8ea14c0162feb810ba9d7cbfe7b2cf1e
Yuusei-FIRE <yu3spk_sm@docomo.ne.jp>
2016-12-05 14:32:32 +0900

    20161121 submit

---
+++ src/perceptron/Perceptron_lib.java
@@ -0,0 +1,39 @@
+package perceptron;
+
+public class Perceptron_lib {
+
+	private int n;
+	private double[] nu;
+	private double epsilon;
+	double theta;
+	public Perceptron_lib(int n, double[] nu, double d, double e) {
+		super();
+		this.n = n;
+		this.nu = nu;
+		this.epsilon = d;
+		this.theta = e;
+	}
+		public double getOutput(double[] x){
+			 
+              double y = 0, output;
+			 for(int i = 0; i < nu.length; i++){
+			 y += nu[i] * x[i];
+			}
+			 if( y > theta) output = 1;
+	           else output = 0;
+			 return output;
+		}
+			 
+
+		public void getParameter(double[] x, double y){
+		
+	 for(int i = 0; i < nu.length; i++){
+	 nu[i] += epsilon * (y - this.getOutput(x))*x[i]; 
+	 }
+ }
+	
+
+
+	}
+	
+
